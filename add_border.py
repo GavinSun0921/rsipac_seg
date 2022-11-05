789b02a548f6636d70e730abe93bf55c92e9aad7
GavinSun0921 <gavinsun0921@foxmail.com>
2022-11-05 11:17:04 +0800

    add file

---
+++ add_border.py
@@ -0,0 +1,27 @@
+import os.path
+
+import cv2
+import numpy as np
+import pandas as pd
+
+dir_test_list_csv = '../dataset/testA_new/test_list.csv'
+dir_pred = './pred'
+dir_export = './submit'
+
+if __name__ == '__main__':
+    border_list = pd.read_csv(dir_test_list_csv)
+
+    if not os.path.exists(dir_export):
+        os.makedirs(dir_export)
+
+    for i in border_list.index:
+        img_name = border_list.iloc[i, 0].values.astype(str)
+        border = border_list.iloc[i, 1:].values.astype(int)
+        x0, y0 = border[0], border[1]
+        x1, y1 = border[2], border[3]
+        x_max, y_max = border[4], border[5]
+
+        mask = np.zeros((y_max, x_max))
+        fill = cv2.imread(os.path.join(dir_pred, img_name), cv2.IMREAD_GRAYSCALE)
+        mask[y0:y1, x0:x1] = fill
+        cv2.imwrite(os.path.join(dir_export, img_name), mask)
